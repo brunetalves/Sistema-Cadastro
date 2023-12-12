dddf8305bcf44ffc2df75f90699a704dec0b8ce1
brunetalves <145602275+brunetalves@users.noreply.github.com>
2023-12-12 16:27:00 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,32 @@
+<?php
+  // Conectar ao banco de dados e recuperar os registros
+  // ...
+
+  // Exibir os registros em uma tabela HTML
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Lista de Registros</title>
+</head>
+<body>
+  <h1>Lista de Registros</h1>
+  <table>
+    <thead>
+      <tr>
+        <!-- Cabeçalho da tabela com os nomes das colunas -->
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        // Loop para exibir os registros na tabela
+        // ...
+      ?>
+    </tbody>
+  </table>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,5 @@
+function validarForm() {
+    // Adicione suas validações aqui
+    // Retorna true se o formulário estiver válido, caso contrário, retorna false
+  }
+  
